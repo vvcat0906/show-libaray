e886a71f41974b9c385a122c46f63fefea8adc05
xuwei <xuwei769518502@qq.com>
2022-09-25 23:39:16 +0800

    sass处理

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,10 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    p {
+      color: red;
+    }
+  }
 </style>
